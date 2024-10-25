95fffe466c2e14314ecce1079ef6c2b66726a194
Sherif Elglaly <sherifelglaly@gmail.com>
2024-10-25 11:40:22 -0700

    add media file

---
+++ untitled/src/Media.java
@@ -0,0 +1,18 @@
+public class Media {
+
+  protected String title;
+  protected enum Genre {    };
+  protected Genre genre;
+  protected  enum MediaType{ };
+  protected MediaType type;
+  
+    public String getTitle() {
+        return title;
+    }
+    public MediaType getMediaType() {
+        return type;
+    }
+    public String toString() {
+        return title;
+    }
+}
